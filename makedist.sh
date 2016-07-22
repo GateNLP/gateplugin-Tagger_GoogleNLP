630169120cf4cfd9595cee8235972dc1ef85713b
Johann Petrak <johann.petrak@gmail.com>
2016-07-22 16:58:44 +0200

    Add build and makedist files.

---
+++ makedist.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+name=gateplugin-Tagger_GoogleNLP
+tmpdir=/tmp
+curdir=`pwd -P`
+version=`perl -n -e 'if (/VERSION="([^"]+)"/) { print $1;}' < $curdir/creole.xml`
+destdir=$tmpdir/${name}$$
+curbranch=`git branch | grep '\*' | cut -c 3-`
+echo Making a release zip for plugin $name, version $version from branch $curbranch
+rm -rf "$destdir"
+mkdir -p $destdir/$name
+rm -f $name-*.zip
+rm -f $name-*.tgz
+git archive --format zip --output ${name}-${version}-src.zip --prefix=$name/ $curbranch
+pushd $destdir
+unzip $curdir/${name}-${version}-src.zip
+cd $name
+cp $curdir/build.properties .
+ant || exit
+ant clean.classes || exit
+rm -rf build.properties
+rm -rf makedist.sh
+rm $curdir/${name}-${version}-src.zip
+cd ..
+zip -r $curdir/$name-$version.zip $name
+echo Created a release zip for plugin $name, version $version from branch $curbranch
+echo Zip file is $curdir/$name-$version.zip
+popd >& /dev/null
